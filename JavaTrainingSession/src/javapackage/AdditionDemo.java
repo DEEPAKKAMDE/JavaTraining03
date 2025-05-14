2b031f2709ac48f7a3f9f4b82d58905374fecd58
DEEPAKKAMDE <deepakkamde03@gmail.com>
2025-05-14 08:29:43 +0530

    This is My Morning Batch Java Programs

---
+++ JavaTrainingSession/src/javapackage/AdditionDemo.java
@@ -0,0 +1,19 @@
+package javapackage;
+
+public class AdditionDemo {
+
+	public static void main(String[] args) {
+
+		int a = 100;
+		int b = 120;
+		float c=99.95f;
+		
+		int sum = a + b;
+		int sub = b - a;
+		System.out.println("Addition of two numbers are = " + sum);
+		System.out.println("Substraction of two numbers are = " + sub);
+		System.out.println(c);
+
+	}
+
+}
---
+++ JavaTrainingSession/src/javapackage/FirstJavaProgram.java
@@ -0,0 +1,13 @@
+package javapackage;
+
+public class FirstJavaProgram {
+
+	
+	public static void main(String[] args) 
+	{
+		System.out.println();
+		System.out.println("Welcome to Automation Testing");
+		System.out.println("Welcome to Tech World");
+	}
+
+}
